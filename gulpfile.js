8717b9312a478592f75c61e6df8694b713d96597
Adrià Fontcuberta <adria@calidae.com>
2018-06-13 07:42:29 +0200

    Automate basic tasks

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const rename = require('gulp-rename')
+const gulpStylelint = require('gulp-stylelint')
+
+const SRC_PATH = 'scss/**/*.scss'
+const OUTPUT_FILE = 'output.css'
+
+gulp.task('sass', () =>
+  gulp
+    .src(SRC_PATH)
+    .pipe(sass())
+    .pipe(rename(OUTPUT_FILE))
+    .pipe(gulp.dest('./'))
+)
+
+gulp.task('stylelint', () =>
+  gulp
+    .src(SRC_PATH)
+    .pipe(gulpStylelint({
+      reporters: [{ formatter: 'string', console: true }]
+    }))
+)
+
+gulp.task('default', ['stylelint', 'sass'])
+
+gulp.task('watch', () => gulp.watch(SRC_PATH, ['default']))
+
